51704e8b4e842c724c2479699d48116b31beca2a
Flotib <flotib@hotmail.fr>
2021-02-06 19:06:17 +0100

    bug corrections + starting the css to display the ticks on channeling bar

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-var progressElement=document.getElementsByClassName("progress")
-console.log(progressElement)
-var i=0
-setInterval(()=>{
-    for(const element of progressElement){
-        element.dataset.label=i+"%"
-    }
-    i++
-},1000)
